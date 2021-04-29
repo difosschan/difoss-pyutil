416dde04b5fa0482dbb9216c8df3d9242a784586
difoss <difoss@163.com>
2021-04-29 18:38:11 +0800

    change LINCESE and imporve author information

--- setup.py
+++ setup.py
@@ -9,9 +9,20 @@ setup(
         'Click',
         'GitPython',
     ],
+    license='Apache License Version 2.0',
+
+    url='https://github.com/difosschan/difoss-pyutil',
+    author='DifossChan',
+    author_mail='difoss@163.com',
     entry_points={
         'console_scripts': {
             "df.cmd = difoss_pyutil.__main__:main"
         },
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
 )
